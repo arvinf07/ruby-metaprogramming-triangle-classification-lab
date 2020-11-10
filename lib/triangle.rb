747c5ebf09e7b1a2089dde60214db1021aea8d99
Arvin Fernandez <arvinf07@gmail.com>
2020-11-10 11:54:56 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
-end
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end  
+
+  def kind
+    sides = [@a, @b, @c]
+    if (@a + @b) <= @c || (@c + @b) <= @a || (@c + @a) <= @b || sides.any? {|side| side <= 0}
+      raise TriangleError
+    elsif sides.all? {|side| side == sides[1]} 
+      :equilateral
+    elsif sides.uniq.count == 2
+      :isosceles
+    else
+      :scalene
+    end  
+  end  
+
+  class TriangleError < StandardError
+    def message
+      "The lengths provided cannot make a triangle"
+    end  
+  end
+
+end 
